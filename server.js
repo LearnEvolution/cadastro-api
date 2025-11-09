852180872d6ea93d9b9f3a41893ba221c3d13ab5
psh.evolution <psh.evolution@gmail.com>
2025-11-08 21:05:32 -0300

    corrigii o cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 
 // ✅ O CORS deve vir logo no início
 app.use(cors({
-  origin: "https://cadastro-frontend-qpx1r0t4v-learnevolutions-projects.vercel.app",
+  origin: "https://cadastro-frontend-4b9wq26xz-learnevolutions-projects.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
